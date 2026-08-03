43be80b56af2e14a0ad3cddc9308f3272d423a67
Sabit Hossen <sabithossen.dev@gmail.com>
2026-08-04 01:19:01 +0600

    create express server

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,6 @@
-// const express = require('express');
-// const app = express()
-// const port = 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
-
-
 import express from "express";
 const app = express();
-const port = 5001;
+const port = 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
