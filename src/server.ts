8b0de4d182e54f7ea2be67c99399f0d3127dea09
Sabit Hossen <sabithossen.dev@gmail.com>
2026-08-06 10:58:06 +0600

    type script add

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import express, { type Application, type Request, type Response } from "express";
+import {Pool} from "pg";
 const app: Application = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+
+const pool = new Pool({
+    
+})
 
 app.get('/', (req: Request, res: Response) => {
     //   res.send('Hello World!');
@@ -13,7 +21,16 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.post('/', async (req: Request , res: Response)=>{
-    console.log(req.body);
+    // console.log(req.body);
+    // const body = req.body;
+    const {name,email,password} = req.body;
+    res.status(201).json({
+        message: "created",
+        data: {
+            name,
+            email
+        },
+    });
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
