846561906f5f7de6441e39ae9366bb9adb863f00
Sabit Hossen <sabithossen.dev@gmail.com>
2026-08-13 04:15:23 +0600

    pos t update

--- src/server.ts
+++ src/server.ts
@@ -92,6 +92,40 @@ app.get('/api/users', async (req: Request, res: Response) => {
     }
 });
 
+app.get('/api/users/:id', async (req: Request, res: Response) => {
+    const {id} = req.params;
+    // console.log(id);
+    try{
+        const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                success: false,
+                message: "user not found",
+                data : {}
+            });
+        };
+        res.status(200).json({
+            success: true,
+            message: "user retrieved successfully",
+            data: result.rows[0],
+        });
+    } catch (error : any){
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        });
+    }
+});
+
+
+app.put('/api/users/:id', async (req: Request, res: Response) =>{
+    const {id} = req.params;
+    const {name,password,age,is_active} = req.body;
+
+    const result = await pool.query(` UPDATE users SET name = $1, password = $2, age = $3, is_active = $4, WHERE id=$5 `, [name, password, age, is_active, id],);
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
