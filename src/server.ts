2ff28d268361525f7359c60ddbc7873a8591954c
Sabit Hossen <sabithossen.dev@gmail.com>
2026-08-14 04:12:52 +0600

    delete post update

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express, { type Application, type Request, type Response } from "express";
 import {Pool} from "pg";
+import config from "./config";
+
 const app: Application = express();
-const port = 8000;
+const port = config.port;
 
 
 app.use(express.json());
@@ -10,7 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 
 
 const pool = new Pool({
-    connectionString : "postgresql://neondb_owner:npg_DRA8GXkaKL2o@ep-mute-violet-ay5ldhrm-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    connectionString : config.connectionString,
+    port : process.env.PORT || 8000,
 });
 
 
@@ -126,7 +129,10 @@ app.put('/api/users/:id', async (req: Request, res: Response) =>{
     try {
         const result = await pool.query(
             `UPDATE users
-             SET name = $1, password = $2, age = $3, is_active = $4
+             SET name =COALESCE($1, name),
+             password = COALESCE($2, password),
+              age = COALESCE($3, age),
+             is_active = COALESCE($4, is_active)
              WHERE id = $5
              RETURNING *`,
             [name, password, age, is_active, id]
@@ -153,6 +159,38 @@ app.put('/api/users/:id', async (req: Request, res: Response) =>{
         });
     }
 });
+
+app.delete('/api/users/:id', async (req: Request, res: Response) =>{
+    const {id} = req.params;
+    const {name,password,age,is_active} = req.body;
+
+    try {
+        const result = await pool.query(
+            `DELETE FROM users
+             WHERE id = $1
+             RETURNING *`,
+            [id]
+        );
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "user deleted successfully",
+                data: {}
+            });
+        }
+        res.status(200).json({
+            success: true,
+            message: "user deleted successfully",
+            data: result.rows[0]
+        });
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+            });
+        }
+})
 
 
 app.listen(port, () => {
